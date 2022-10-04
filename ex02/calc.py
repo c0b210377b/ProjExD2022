0f95dc45f14920b1d11e4b807b15b2dd7d651a6b
Mitsuemon <c0b210377b@edu.teu.ac.jp>
2022-10-04 15:35:26 +0900

    電卓7段階

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,9 +3,15 @@ import tkinter.messagebox as tkm
 
 def click_number(event):
     btn = event.widget
-    num = (btn["text"])
+    num = btn["text"]
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
     entry.insert(tk.END, num)
+
+def click_equal(event):
+    eqn = entry.get()
+    result = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, result)
 
 den = tk.Tk()
 den.title("電卓")
@@ -17,8 +23,7 @@ entry.grid(row = 0, column = 0, columnspan = 3)
 r, c = 1, 0
 num_list = list(range(9, -1, -1))
 add_btn = ["+"]
-equal_btn = ["="]
-for i, num in enumerate(num_list+add_btn+equal_btn, 1):
+for i, num in enumerate(num_list+add_btn, 1):
     button = tk.Button(den, text = f"{num}", 
     font=("Times New Roman", 30), width = 4, height = 2)
     button.bind("<1>", click_number)
@@ -28,4 +33,8 @@ for i, num in enumerate(num_list+add_btn+equal_btn, 1):
         r += 1
         c = 0
     
+btn = tk.Button(den, text=f"=", font=("Times New Roman", 30), width = 4, height = 2)
+btn.bind("<1>", click_equal)
+btn.grid(row = r, column = c)
+
 den.mainloop()

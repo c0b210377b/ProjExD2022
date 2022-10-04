fee30f65147eafc6cb70008111819818020d21fa
Mitsuemon <c0b210377b@edu.teu.ac.jp>
2022-10-04 15:17:31 +0900

    電卓7段階(途中)

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,20 +3,24 @@ import tkinter.messagebox as tkm
 
 def click_number(event):
     btn = event.widget
-    num = int(btn["text"])
+    num = (btn["text"])
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
     entry.insert(tk.END, num)
 
 den = tk.Tk()
 den.title("電卓")
-den.geometry("300x500")
+den.geometry("300x555")
 
 entry = tk.Entry(den, width = 10, font = ("", 40), justify = "right")
 entry.grid(row = 0, column = 0, columnspan = 3)
 
 r, c = 1, 0
-for i, num in enumerate(range(9, -1, -1), 1):
-    button = tk.Button(den, text = f"{num}", font=("Times New Roman", 30), width = 4, height = 2)
+num_list = list(range(9, -1, -1))
+add_btn = ["+"]
+equal_btn = ["="]
+for i, num in enumerate(num_list+add_btn+equal_btn, 1):
+    button = tk.Button(den, text = f"{num}", 
+    font=("Times New Roman", 30), width = 4, height = 2)
     button.bind("<1>", click_number)
     button.grid(row=r, column = c)
     c += 1
@@ -24,8 +28,4 @@ for i, num in enumerate(range(9, -1, -1), 1):
         r += 1
         c = 0
     
-
-#button_1 = tk.Button(den, text="1", font=("Times New Roman", 30))
-
-
 den.mainloop()

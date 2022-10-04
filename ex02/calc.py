991f628f03d93b913e44b52033fd07422c9d5956
Mitsuemon <c0b210377b@edu.teu.ac.jp>
2022-10-04 16:38:34 +0900

    劣化改良電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,23 +7,26 @@ def click_number(event):
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
     entry.insert(tk.END, num)
 
-def click_equal(event):
+def click_equal(evenet):
     eqn = entry.get()
     result = eval(eqn)
     entry.delete(0, tk.END)
     entry.insert(tk.END, result)
 
+def click_delete(event):
+    entry.delete(0, tk.END)
+
 den = tk.Tk()
 den.title("電卓")
-den.geometry("300x555")
+den.geometry("400x800")
 
 entry = tk.Entry(den, width = 10, font = ("", 40), justify = "right")
 entry.grid(row = 0, column = 0, columnspan = 3)
 
-r, c = 1, 0
-num_list = list(range(9, -1, -1))
-add_btn = ["+"]
-for i, num in enumerate(num_list+add_btn, 1):
+r, c = 2, 0
+num_list = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "00"]
+ope1_btn = ["."]
+for i, num in enumerate(num_list+ope1_btn, 1):
     button = tk.Button(den, text = f"{num}", 
     font=("Times New Roman", 30), width = 4, height = 2)
     button.bind("<1>", click_number)
@@ -32,9 +35,19 @@ for i, num in enumerate(num_list+add_btn, 1):
     if i % 3 == 0:
         r += 1
         c = 0
-    
+
+btn = tk.Button(den, text = "C", font = ("Times New Roman", 30), width = 4, height = 2)
+btn.bind("<1>", click_delete)
+btn.grid(row = 0, column = 4)
+
+en_lst = ["+", "-", "*", "/"]
+for i, en in enumerate(en_lst, 2):
+    btn = tk.Button(den, text=en, font=("Times New Roman", 30), width = 4, height = 2)
+    btn.bind("<1>", click_number)
+    btn.grid(row = i, column = 4)
+
 btn = tk.Button(den, text=f"=", font=("Times New Roman", 30), width = 4, height = 2)
 btn.bind("<1>", click_equal)
-btn.grid(row = r, column = c)
+btn.grid(row = r, column = c+4)
 
 den.mainloop()

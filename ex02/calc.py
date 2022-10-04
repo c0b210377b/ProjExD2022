e14f3ae642274ec4bdb751f68acab51dcb2193ce
Mitsuemon <c0b210377b@edu.teu.ac.jp>
2022-10-04 14:58:05 +0900

    電卓5段階

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,13 +4,17 @@ import tkinter.messagebox as tkm
 def click_number(event):
     btn = event.widget
     num = int(btn["text"])
-    tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    entry.insert(tk.END, num)
 
 den = tk.Tk()
 den.title("電卓")
 den.geometry("300x500")
 
-r, c = 0, 0
+entry = tk.Entry(den, width = 10, font = ("", 40), justify = "right")
+entry.grid(row = 0, column = 0, columnspan = 3)
+
+r, c = 1, 0
 for i, num in enumerate(range(9, -1, -1), 1):
     button = tk.Button(den, text = f"{num}", font=("Times New Roman", 30), width = 4, height = 2)
     button.bind("<1>", click_number)

99bee3fb55e667ffccc02ef47f5e8213515aa7a6
Mitsuemon <c0b210377b@edu.teu.ac.jp>
2022-10-18 14:14:00 +0900

    練習段階2

--- ex03/fig/maze.py
+++ ex03/fig/maze.py
@@ -1,4 +1,9 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-from maze_maker import *
+#from maze_maker import *
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+
+    root.mainloop()

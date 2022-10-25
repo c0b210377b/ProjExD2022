98c3b1cbda35c86ba3d9545cfa907712d9bc4e40
Mitsuemon <c0b210377b@edu.teu.ac.jp>
2022-10-25 15:01:22 +0900

    練習5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,26 +6,31 @@ import sys
 def main():
     pg.display.set_caption("KFCから逃げろやｯｯ!!こうかとん")
     scrn = pg.display.set_mode((1600, 900))
-
+    scrn_r = scrn.get_rect()
     r = rm.randint(0, 9)
     
     back = pg.image.load("./ex04/pg_bg.jpg")        #背景画像
     back_r = back.get_rect()
 
-    tori = pg.image.load(f"./ex04/fig/{r}.png") 
+    tori = pg.image.load(f"./ex04/fig/{r}.png")     #こうかとん画像
     tori = pg.transform.rotozoom(tori, 0, 2)
     tori_r = tori.get_rect()
     tori_r.center = 900, 400
 
-    kfc = pg.image.load("./ex04/KFC.jpg")
+    kfc = pg.image.load("./ex04/KFC.jpg")           #カーネルサンダース
     kfc = pg.transform.rotozoom(kfc, 0, 0.3)
 
-    clock =  pg.time.Clock()    #練習1
+    bomb = pg.Surface((20, 20))
+    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10) #円を描く
+    bomb.set_colorkey((0, 0, 0))                    #四隅の黒い部分を等価させる
+    bomb_r = bomb.get_rect()
+    bomb_r.centerx, bomb_r.centery = rm.randint(0, scrn_r.width), rm.randint(0, scrn_r.height)
+
+    clock =  pg.time.Clock()    
 
     while True:
-        scrn.blit(back, back_r)         #スクリーンにこうかとんを貼る
-        scrn.blit(tori, tori_r)
-        scrn.blit(kfc, (200, 500))
+        scrn.blit(back, back_r)         #スクリーンに背景を貼る
+        scrn.blit(kfc, (200, 500))      
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -40,7 +45,9 @@ def main():
             tori_r.centerx -= 1
         if key_state[pg.K_RIGHT]:       #こうかとんの横座標を+1
             tori_r.centerx += 1
+        scrn.blit(tori, tori_r)         #スクリーンにこうかとんを貼る
 
+        scrn.blit(bomb, bomb_r)
         
         pg.display.update()
         clock.tick(1000)

7d11a881430210439a5bd2bb65f2dbcc1f263d66
Mitsuemon <c0b210377b@edu.teu.ac.jp>
2022-10-25 14:19:32 +0900

    練習3修正

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,14 +6,19 @@ import sys
 def main():
     pg.display.set_caption("KFCから逃げろやｯｯ!!こうかとん")
     scrn = pg.display.set_mode((1600, 900))
-    
+
     r = rm.randint(0, 9)
-    kfc = pg.image.load("./ex04/KFC.jpg")
-    back = pg.image.load("./ex04/pg_bg.jpg")
-    tori = pg.image.load(f"./ex04/fig/{r}.png")
-    kfc = pg.transform.rotozoom(kfc, 0, 0.25)
+    
+    back = pg.image.load("./ex04/pg_bg.jpg")        #背景画像
+    back_r = back.get_rect()
+
+    tori = pg.image.load(f"./ex04/fig/{r}.png") 
     tori = pg.transform.rotozoom(tori, 0, 2)
-    back = pg.transform.rotozoom(back, 0, 1)
+    tori_r = tori.get_rect()
+    tori_r.center = 800, 450
+
+    kfc = pg.image.load("./ex04/KFC.jpg")
+    kfc = pg.transform.rotozoom(kfc, 0, 0.25)
 
     key_lst = pg.key.get_pressed()
     
@@ -21,14 +26,17 @@ def main():
     while True:
         pg.display.update()
         pg.time.Clock().tick(1000)
-        scrn.blit(back, (0, 0))
-        scrn.blit(tori, (900, 400))
+        scrn.blit(back, back_r)
+        scrn.blit(tori, tori_r)
         scrn.blit(kfc, (200, 500))
 
         for event in pg.event.get():
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_x:
-                    return
+            if event.type == pg.QUIT:
+                return
+
+            # if event.type == pg.KEYDOWN:
+            #     if event.key == pg.K_x:
+            #         return
                 
 
 if __name__ == "__main__":

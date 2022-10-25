da4f8d0a1d821e69a9e3115007a76dba5232f9b7
Mitsuemon <c0b210377b@edu.teu.ac.jp>
2022-10-25 11:43:11 +0900

    第4回

---
+++ ex04/sample.py
@@ -0,0 +1,12 @@
+import pygame as pg
+import sys
+
+#def main():
+
+if __name__ == "__main__":
+    pg.init()
+    # main()
+    pg.display.set_caption("初めてのPyGame")
+    scrn_sfc = pg.display.set_mode((800, 600))
+    pg.quit()
+    sys.exit()
